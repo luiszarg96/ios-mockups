f248fe51108e83614688e5673292590e84e5e751
Luis Zarza <luis.zarza01@gmail.com>
2020-04-01 13:00:29 -0700

    Initial Commit

---
+++ bootcamp-homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bootcamp-homework
+//
+//  Created by Mobile Roshka on 4/1/20.
+//  Copyright © 2020 Mobile Roshka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bootcamp-homeworkTests/bootcamp_homeworkTests.swift
@@ -0,0 +1,34 @@
+//
+//  bootcamp_homeworkTests.swift
+//  bootcamp-homeworkTests
+//
+//  Created by Mobile Roshka on 4/1/20.
+//  Copyright © 2020 Mobile Roshka. All rights reserved.
+//
+
+import XCTest
+@testable import bootcamp_homework
+
+class bootcamp_homeworkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
